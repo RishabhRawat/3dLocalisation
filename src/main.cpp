4a832bfd8cdae7696e35aaa3ae309802d9a6cce2
Rishabh Rawat <rishabhrawat1994@gmail.com>
2015-09-27 00:41:55 +0530

    Localisation using recoverpose

--- src/main.cpp
+++ src/main.cpp
@@ -51,17 +51,21 @@ int main(int argc, char** argv)
     double camdata[3][3] = { { 535.4, 0, 320.1 }, { 0, 539.2, 247.6 }, { 0, 0, 1 } };
     Mat IntCamMat = Mat(3, 3, CV_64F, camdata);
 	Mat x = Mat::zeros(3, 1, CV_64F);
+	Mat y;
 	
     Mat img_object_1 = imread(string(argv[1])+FileList[0], CV_LOAD_IMAGE_GRAYSCALE);
     detector->detect(img_object_1, keypoints_object_1);
     extractor->compute(img_object_1, keypoints_object_1, descriptors_object_1);
 	namedWindow( "MyImage", WINDOW_AUTOSIZE );
+	namedWindow( "Plot", WINDOW_AUTOSIZE );
+	
+	Mat plot(640, 480, CV_8UC3, Scalar(0,0,0));
 
     for (unsigned int i = 1; i < FileList.size(); i++) {
 		cout<<i<<endl;
 		Mat img_object_2 = imread(string(argv[1])+FileList[i], CV_LOAD_IMAGE_GRAYSCALE);
 		imshow("MyImage",img_object_2);
-		waitKey(25);
+		waitKey(13);
 		if (!img_object_1.data || !img_object_2.data) {
 			std::cout << " --(!) Error reading images " << std::endl;
 			return -1;
@@ -111,22 +115,36 @@ int main(int argc, char** argv)
 			object_1.push_back(keypoints_object_1[good_matches[i].queryIdx].pt);
 			object_2.push_back(keypoints_object_2[good_matches[i].trainIdx].pt);
 		}
+		Mat E, R, t, masks;
 		
-		Mat H = findHomography(object_1, object_2, CV_RANSAC);
-		if(H.data == NULL)
-		{
-				cout<<"Couldn't Find Homography";
-				continue;
-		}
-				
+		E = findEssentialMat(object_1, object_2, 537, Point2f(320.1, 247.6), RANSAC, 0.999, 1.0, masks);
+		recoverPose(E, object_1, object_2, R, t, 537, Point2f(320.1, 247.6), masks);
+		y = x+t;
+		cout<<t<<endl;
+		cout<<t.at<double>(0)<<" "<<t.at<double>(2)<<endl;
+		cout<<Point2d(t.at<double>(0),t.at<double>(2))<<endl;
+		line(plot,Point2d(3*x.at<double>(0,0)+127,3*x.at<double>(0,2)+127),Point2d(3*y.at<double>(0,0)+127,3*y.at<double>(0,2)+127),Scalar(255,255,255));
+		imshow("Plot",plot);
+		waitKey(13);
+		//cout<<t;
 
-		// OutputArrayOfArrays rot, trans, scale;
-		std::vector<Mat> rot, trans, scale;
-		decomposeHomographyMat(H, IntCamMat, rot, trans, scale);
+
+
+//		Mat H = findHomography(object_1, object_2, CV_RANSAC);
+//		if(H.data == NULL)
+//		{
+//				cout<<"Couldn't Find Homography";
+//				continue;
+//		}
+//				
+//
+//		// OutputArrayOfArrays rot, trans, scale;
+//		std::vector<Mat> rot, trans, scale;
+//		decomposeHomographyMat(H, IntCamMat, rot, trans, scale);
 		
 		// TODO correct this and plot
-		x = x+trans[0];
-		cout<<x;
+		x = x+t;
+		//cout<<x<<endl;
 		
 		descriptors_object_1 = descriptors_object_2;
 		keypoints_object_1 = keypoints_object_2;
